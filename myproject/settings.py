74b832ae3828547a88ff976c7b3519a28e2d2f69
haoranzues <haoranzeus@163.com>
2017-04-08 09:36:57 +0800

    close debug mode in environment parameter

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jy_j^j_)wkdpf8!(0-g9=4^tad8a6-u+tg#lj*@rs8)a6$6dy$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
 
