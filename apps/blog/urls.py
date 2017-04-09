0c8c3ad0c4aabce4cb0b50c9b5f8854e1ecf45e8
haoranzues <haoranzeus@163.com>
2017-04-09 21:45:41 +0800

    urls add /?$

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,16 +3,16 @@ from apps.blog import views
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^login/', views.LoginView.as_view(), name='login'),
-    url(r'^logout/', views.LogOutView.as_view(), name='logout'),
-    url(r'^register/', views.RegisterView.as_view(), name='register'),
+    url(r'^login/?$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/?$', views.LogOutView.as_view(), name='logout'),
+    url(r'^register/?$', views.RegisterView.as_view(), name='register'),
 
-    url(r'^index/', views.IndexCenterView.as_view(), name='indexcenter'),
+    url(r'^index/?$', views.IndexCenterView.as_view(), name='indexcenter'),
     url(r'^indexclassify/(?P<classify>[0-9]+)/?$',
         views.IndexClassify.as_view(), name='indexclassify'),
-    url(r'^timeline/', views.TimeLineView.as_view(), name='timeline'),
+    url(r'^timeline/?$', views.TimeLineView.as_view(), name='timeline'),
     url(r'^article/(?P<art_date>[0-9]{8})/(?P<pinyin_title>.+)/?$',
         views.ArticleView.as_view(), name='article'),
 
-    url(r'^test/', views.testview, name='testview'),
+    url(r'^test/?$', views.testview, name='testview'),
 ]
